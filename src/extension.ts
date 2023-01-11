5c958dffcf5df2d4d42229adf8cebef7ad1931ad
Ion Utale <utale.ion@gmail.com>
2023-01-11 05:31:42 +0100

    retreive info from git and display on a notification message

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as git from './git';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -9,18 +10,41 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "git-stuff" is now active!');
+	const currentFile = vscode.window.activeTextEditor?.document.fileName.split('/').pop() ?? "no active editor";
+	const workspaceDirectory = vscode.workspace.workspaceFolders?.[0].uri.fsPath ?? "no workspace";
+	const cursorLine = vscode.window.activeTextEditor?.selection.active.line ?? 1;
 
+
+	console.table([
+		{
+			name: "Workspace Directory",
+			value: workspaceDirectory
+		},
+		{
+			name: "Cursor Is On Line",
+			value: cursorLine
+		},
+		{
+			name: "Active File",
+			value: currentFile
+		}]);
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('git-stuff.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from git-stuff!');
+		vscode.window.showInformationMessage('Hello World vs-code!');
 	});
 
 	context.subscriptions.push(disposable);
+
+	const lineBlame = git.getGitLineBlame(workspaceDirectory, currentFile, cursorLine + 1);
+	console.log("lineBlame: " + lineBlame);
+
+	vscode.window.showInformationMessage(lineBlame);
+
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
---
+++ src/git.ts
@@ -0,0 +1,27 @@
+import * as cp from 'child_process';
+
+	/*
+	git terminal commands:
+	- git blame -L 1,1 src/extension.ts
+	- git show -s --format=%s 4932a1cc8c64a5b66b775a2e6856355b9316340b
+	 or
+	 git show -s --format="%an,%at,%s" 4932a1cc8c64a5b66b775a2e6856355b9316340b
+	*/
+  
+export function getGitLineBlame(workspacePath: string, filePath: string, line: number): string {
+  const gitBlameCommand = `git blame -L ${line},${line} ${filePath}`;
+  const gitBlameOutput = cp.execSync(gitBlameCommand,
+    { cwd: workspacePath}
+    ).toString();
+  const gitBlameLineHash = gitBlameOutput.split(' ')[0];
+
+
+  const gitShowCommand = `git show -s --format="%an,%at,%s" ${gitBlameLineHash}`;
+  const gitShowOutput = cp.execSync(gitShowCommand,
+    { cwd: workspacePath}
+    ).toString();
+    // console.log(gitShowOutput);
+  const gitBlameLine = gitShowOutput;
+
+  return gitBlameLine ?? '';
+}
