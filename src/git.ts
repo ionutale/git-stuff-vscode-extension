5e3259d43ef8419f9043dea54419ecba888444e9
Ion Utale <utale.ion@gmail.com>
2023-01-14 11:31:12 +0100

    added relative time calculation for each commit

--- src/git.ts
+++ src/git.ts
@@ -1,32 +1,40 @@
 import * as cp from 'child_process';
+import * as Moment from 'moment';
 
-	/*
-	git terminal commands:
-	- git blame -L 1,1 src/extension.ts
-	- git show -s --format=%s 4932a1cc8c64a5b66b775a2e6856355b9316340b
-	 or
-	 git show -s --format="%an,%at,%s" 4932a1cc8c64a5b66b775a2e6856355b9316340b
-	*/
-  
+
+/*
+git terminal commands:
+- git blame -L 1,1 src/extension.ts
+- git show -s --format=%s 4932a1cc8c64a5b66b775a2e6856355b9316340b
+ or
+ git show -s --format="%an,%at,%s" 4932a1cc8c64a5b66b775a2e6856355b9316340b
+*/
+
 export function getGitLineBlame(workspacePath: string, filePath: string, line: number): string {
   try {
-    
+
     const gitBlameCommand = `git blame -L ${line},${line} ${filePath}`;
     const gitBlameOutput = cp.execSync(gitBlameCommand,
-      { cwd: workspacePath}
-      ).toString();
-      const gitBlameLineHash = gitBlameOutput.split(' ')[0];
-      
-      
-      const gitShowCommand = `git show -s --format="%an,%at,%s" ${gitBlameLineHash}`;
-      const gitShowOutput = cp.execSync(gitShowCommand,
-        { cwd: workspacePath}
-        ).toString();
-        // console.log(gitShowOutput);
-        const gitBlameLine = gitShowOutput;
-        
-        return gitBlameLine ?? '';
-      } catch (error) {
-        return 'modified by you';
-      }
+      { cwd: workspacePath }
+    ).toString();
+    const gitBlameLineHash = gitBlameOutput.split(' ')[0];
+
+    const gitShowCommand = `git show -s --format="%an,%at,%s" ${gitBlameLineHash}`;
+    const gitShowOutput = cp.execSync(gitShowCommand,
+      { cwd: workspacePath }
+    ).toString();
+    // console.log(gitShowOutput);
+    const [name, epoch, message] = gitShowOutput.split(',');
+    const relativeTime = epochToRelativeTime(Number(epoch));
+    const gitBlameLine = `${name}, ${relativeTime}: ${message}`;
+
+    return gitBlameLine ?? '';
+  } catch (error) {
+    return 'uncommited line';
+  }
+}
+
+function epochToRelativeTime(epoch: number): string {
+  const moment = Moment.unix(epoch);
+  return moment.fromNow();
 }
